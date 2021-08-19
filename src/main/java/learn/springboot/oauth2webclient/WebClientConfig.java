7dbfc97e49b00651ee484c54a216949e8968e1be
jainaks2010 <jainaks2010@gmail.com>
2021-08-19 19:09:53 +0530

    Removing unwanted code for Okta Auth

--- src/main/java/learn/springboot/oauth2webclient/WebClientConfig.java
+++ src/main/java/learn/springboot/oauth2webclient/WebClientConfig.java
@@ -3,13 +3,10 @@ package learn.springboot.oauth2webclient;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.oauth2.client.oidc.web.logout.OidcClientInitiatedLogoutSuccessHandler;
 import org.springframework.security.oauth2.client.registration.ClientRegistrationRepository;
-import org.springframework.security.web.authentication.logout.LogoutSuccessHandler;
 
 @Configuration
 @ConfigurationProperties(prefix = "coupon.resource")
@@ -17,12 +14,6 @@ public class WebClientConfig extends WebSecurityConfigurerAdapter {
 
     @Autowired
     private ClientRegistrationRepository clientRegistrationRepository;
-
-   /* @Override
-    public void configure(WebSecurity web) throws Exception {
-        web.ignoring().mvcMatchers(HttpMethod.GET, "/home/**");
-        super.configure(web);
-    }*/
 
 
 
@@ -41,7 +32,7 @@ public class WebClientConfig extends WebSecurityConfigurerAdapter {
             .invalidateHttpSession(true)
             .clearAuthentication(true)
             .deleteCookies("JSESSIONID");
-        //http.authorizeRequests().mvcMatchers("/app/logout").permitAll().and().logout().logoutSuccessHandler(logoutSuccessHandler());
+        
     }
 
     private OidcClientInitiatedLogoutSuccessHandler logoutSuccessHandler() {
